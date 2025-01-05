200bd1b7fdeb11617f6d4a86c4247e2fad44fdb1
Sangwon Lee <samlee0326@gmail.com>
2025-01-05 23:55:12 +0900

    Delete conda-django directory

--- conda-django/mysite/mysite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('rest/', include('polls_api.urls')),
-]
